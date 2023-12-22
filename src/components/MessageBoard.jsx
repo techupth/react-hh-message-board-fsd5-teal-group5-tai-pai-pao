17715d23eb94de7cb2d745b63eba04ca56ae941c
Tai <5506726tai@gmail.com>
2023-12-22 11:20:07 +0700

    Ex finish

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [mesInput, setMesIput] = useState("");
+  const [mesList, setMesList] = useState([]);
+
+  const handAddMesList = () => {
+    const newMesList = [...mesList];
+
+    newMesList.push(mesInput);
+
+    setMesList(newMesList);
+  };
+
   return (
     <div className="app-wrapper">
       <h1 class="app-title">Message board</h1>
@@ -9,15 +22,25 @@ function MessageBoard() {
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            onChange={(e) => {
+              setMesIput(e.target.value);
+            }}
+            value={mesInput}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button" onClick={handAddMesList}>
+          Submit
+        </button>
       </div>
       <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+        {mesList.map((mes, index) => {
+          return (
+            <div key={index} className="message">
+              <h1>{mes}</h1>
+              <button className="delete-button">x</button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
